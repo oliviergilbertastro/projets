69772119e01918f55dbc201428bc15f232690ff0
Olivier Gilbert <58561270+oliviergilbertulaval@users.noreply.github.com>
2024-01-16 14:47:44 -0500

    first files

---
+++ double_pendulum_matplotlib.py
@@ -0,0 +1,162 @@
+import numpy as np
+from time import sleep
+from random import random
+import sympy as sp
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+from matplotlib.animation import FuncAnimation
+from tqdm import tqdm
+import itertools
+
+
+
+#Draw double pendulum
+class Pendulum():
+    def __init__(self, l1, l2, m1, m2, theta1_0, theta2_0, v1_0, v2_0):
+        self.m1 = m1
+        self.l1 = l1
+        self.m2 = m2
+        self.l2 = l2
+        self.conds = [theta1_0, v1_0, theta2_0, v2_0]
+    
+    def realtime(self, fps, tres=0.03):
+        acc1, acc2 = self.solve_pendulum()
+        l = self.l1+self.l2
+        fig = plt.figure()
+        ax = plt.axes(xlim=(-l, l), ylim=(-l, l))
+        ax.set_aspect('equal')
+        ax.tick_params(left = False, right = False , labelleft = False , 
+                labelbottom = False, bottom = False)
+        ax.set_aspect('equal')
+        self.theta1 = self.conds[0]
+        self.dtheta1 = self.conds[1]
+        self.theta2 = self.conds[2]
+        self.dtheta2 = self.conds[3]
+        ropes, = ax.plot([0, sp.sin(self.theta1)*self.l1, (sp.sin(self.theta1)*self.l1+sp.sin(self.theta2)*self.l2)], [0, -sp.cos(self.theta1)*self.l1, (-sp.cos(self.theta1)*self.l1-sp.cos(self.theta2)*self.l2)],'-', lw=3, color='black')
+        pendulums, = ax.plot([sp.sin(self.theta1)*self.l1, (sp.sin(self.theta1)*self.l1+sp.sin(self.theta2)*self.l2)], [-sp.cos(self.theta1)*self.l1, (-sp.cos(self.theta1)*self.l1-sp.cos(self.theta2)*self.l2)], 'o', color='red')
+        pen2x = [(sp.sin(self.theta1)*self.l1+sp.sin(self.theta2)*self.l2)]
+        pen2y = [(-sp.cos(self.theta1)*self.l1-sp.cos(self.theta2)*self.l2)]
+        history, = ax.plot(pen2x, pen2y, '--', lw=2, color='pink')
+
+        # animation function.  This is called sequentially
+        def init():
+            ropes.set_data([],[])
+            pendulums.set_data([],[])
+            history.set_data(pen2x, pen2y)
+            return [ropes, pendulums, history]
+        
+        def update(i):
+            
+            self.theta1 += self.dtheta1*tres
+            self.theta2 += self.dtheta2*tres
+            self.dtheta1 += acc1(9.81, self.l1, self.l2, self.m1, self.m2, self.theta1, self.theta2, self.dtheta1, self.dtheta2).simplify()*tres
+            self.dtheta2 += acc2(9.81, self.l1, self.l2, self.m1, self.m2, self.theta1, self.theta2, self.dtheta1, self.dtheta2).simplify()*tres
+            pen2x.append((sp.sin(self.theta1)*self.l1+sp.sin(self.theta2)*self.l2))
+            pen2y.append((-sp.cos(self.theta1)*self.l1-sp.cos(self.theta2)*self.l2))
+            ropes.set_data([0, sp.sin(self.theta1)*self.l1, (sp.sin(self.theta1)*self.l1+sp.sin(self.theta2)*self.l2)], [0, -sp.cos(self.theta1)*self.l1, (-sp.cos(self.theta1)*self.l1-sp.cos(self.theta2)*self.l2)])
+            pendulums.set_data([sp.sin(self.theta1)*self.l1, (sp.sin(self.theta1)*self.l1+sp.sin(self.theta2)*self.l2)], [-sp.cos(self.theta1)*self.l1, (-sp.cos(self.theta1)*self.l1-sp.cos(self.theta2)*self.l2)])
+            history.set_data(pen2x, pen2y)
+            return [ropes, pendulums, history]
+
+        anim = FuncAnimation(
+                               fig, 
+                               update, 
+                               init_func=init,
+                               frames = None,
+                               interval = 1000/fps, # in ms
+                               )
+        plt.show()
+
+
+    def preanimate(self, frames, fps, tres=0.03):
+        acc1, acc2 = self.solve_pendulum()
+        posx = []
+        posy = []
+        self.theta1 = self.conds[0]
+        self.dtheta1 = self.conds[1]
+        self.theta2 = self.conds[2]
+        self.dtheta2 = self.conds[3]
+        l = self.l1+self.l2
+        for i in tqdm(range(frames+1)):
+            posx.append([0, sp.sin(self.theta1)*self.l1, (sp.sin(self.theta1)*self.l1+sp.sin(self.theta2)*self.l2)])
+            posy.append([0, -sp.cos(self.theta1)*self.l1, (-sp.cos(self.theta1)*self.l1-sp.cos(self.theta2)*self.l2)])
+            self.theta1 += self.dtheta1*tres
+            self.theta2 += self.dtheta2*tres
+            self.dtheta1 += acc1(9.81, self.l1, self.l2, self.m1, self.m2, self.theta1, self.theta2, self.dtheta1, self.dtheta2).simplify()*tres
+            self.dtheta2 += acc2(9.81, self.l1, self.l2, self.m1, self.m2, self.theta1, self.theta2, self.dtheta1, self.dtheta2).simplify()*tres
+
+        
+        fig = plt.figure()
+        ax = plt.axes(xlim=(-l, l), ylim=(-l, l))
+        ax.set_aspect('equal')
+        ax.tick_params(left = False, right = False , labelleft = False , 
+                labelbottom = False, bottom = False)
+        
+        ropes, = ax.plot([0, sp.sin(self.theta1)*self.l1, (sp.sin(self.theta1)*self.l1+sp.sin(self.theta2)*self.l2)], [0, -sp.cos(self.theta1)*self.l1, (-sp.cos(self.theta1)*self.l1-sp.cos(self.theta2)*self.l2)],'-', color='black')
+        pendulums, = ax.plot([sp.sin(self.theta1)*self.l1, (sp.sin(self.theta1)*self.l1+sp.sin(self.theta2)*self.l2)], [-sp.cos(self.theta1)*self.l1, (-sp.cos(self.theta1)*self.l1-sp.cos(self.theta2)*self.l2)], 'o', color='red')
+        # animation function.  This is called sequentially
+        def init():
+            ropes.set_data([],[])
+            pendulums.set_data([],[])
+            return [ropes, pendulums]
+        
+        def update(i):
+            
+            ropes.set_data(posx[i], posy[i])
+            pendulums.set_data(posx[i][1:], posy[i][1:])
+            return [ropes, pendulums]
+
+        anim = FuncAnimation(
+                               fig, 
+                               update, 
+                               init_func=init,
+                               frames = frames,
+                               interval = 1000/fps, # in ms
+                               )
+        plt.show()
+
+    def solve_pendulum(self):
+        m1, m2, g, l1, l2, t = sp.symbols(('m1', 'm2', 'g', 'l1', 'l2', 't'))
+        theta1 = sp.Function('theta1')(t)
+        dtheta1 = theta1.diff(t)
+        ddtheta1 = dtheta1.diff(t)
+        theta2 = sp.Function('theta2')(t)
+        dtheta2 = theta2.diff(t)
+        ddtheta2 = dtheta2.diff(t)
+
+        x1,y1 = l1*sp.sin(theta1), -l1*sp.cos(theta1)
+        x2,y2 = l1*sp.sin(theta1)+l2*sp.sin(theta2), -l1*sp.cos(theta1)-l2*sp.cos(theta2)
+        T = sp.Rational(1,2)*m1*(x1.diff(t)**2+y1.diff(t)**2)+sp.Rational(1,2)*m2*(x2.diff(t)**2+y2.diff(t)**2)
+        V = m1*g*y1+m2*g*y2
+        L = T-V
+
+        #Euler-Lagrange
+        lhs1 = L.diff(theta1)
+        rhs1 = sp.diff(L.diff(dtheta1), t)
+
+        eulerLagrange1 = rhs1-lhs1
+        eulerLagrange1 = sp.solve(eulerLagrange1, ddtheta1)[0]
+
+        acc1 = sp.lambdify((g,l1,l2,m1,m2,theta1,theta2,dtheta1,dtheta2), eulerLagrange1, modules=sp)
+
+        lhs2 = L.diff(theta2)
+        rhs2 = sp.diff(L.diff(dtheta2), t)
+
+        eulerLagrange2 = rhs2-lhs2
+        eulerLagrange2 = sp.solve(eulerLagrange2, ddtheta2)[0]
+        print(eulerLagrange1, eulerLagrange2)
+        acc2 = sp.lambdify((g,l1,l2,m1,m2,theta1,theta2,dtheta1,dtheta2), eulerLagrange2, modules=sp)
+        return acc1, acc2
+
+
+
+#initialize pendulum:
+#pendule = Pendulum(1, 1, 1, 1, random()*np.pi/2-np.pi/4, random()*np.pi-np.pi/2, random()*10-5, random()*10-5)
+pendule = Pendulum(2, 1, 1, 1, 2, 2, 0, 0)
+#Animate:
+pendule.realtime(60, tres=0.02)
+#pendule.preanimate(1000, 30, 0.03)
+
+
+
---
+++ gravity_fluctuations.py
@@ -0,0 +1,186 @@
+import numpy as np
+from time import sleep
+from random import random, gauss
+import sympy as sp
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+from matplotlib.animation import FuncAnimation
+from tqdm import tqdm
+
+G = 6.6743E-11
+
+def mass_random(width, height, mean_mass, resolution, sigma):
+    number_of_squares = (width*height)/(resolution**2)
+    value = gauss(mean_mass, sigma)
+    return value if value > 0 else 0
+
+def distance(x1,y1,x2,y2):
+    return np.sqrt((x1-x2)**2+(y1-y2)**2)
+
+#Pendulum
+class Universe():
+    def __init__(self, width, height, mean_mass, resolution=1, sigma=1, tres=1):
+        '''
+        Create a square 2D universe with almost perfectly isotropic mass (energy) distribution
+        '''
+        self.width = width
+        self.height = height
+        self.tres = tres
+        self.res = resolution
+        self.grid = (int(width/resolution),int(height/resolution))
+        x, y = np.meshgrid(np.arange(0,width, resolution), np.arange(0,height, resolution))
+        #Initialize a randomly distributed spacetime
+        self.spacetime = []
+        for i in range(int(width/resolution)*int(height/resolution)):
+            self.spacetime.append(mass_random(width,height,mean_mass,resolution,sigma))
+        self.spacetime = np.array(self.spacetime).reshape(int(width/resolution),int(height/resolution))
+        self.inispacetime = self.spacetime.copy()
+        #Initialize velocity grids (x and y) - static at t=0
+        self.vx = np.zeros(self.grid)
+        self.vy = np.zeros(self.grid)
+
+    
+
+    def show(self):
+        ax = plt.subplot(111)
+        plt.imshow(self.spacetime, cmap="inferno", origin='lower')
+
+        ticklabels = ax.get_xticklabels()
+        ticklabels.extend( ax.get_yticklabels() )
+        for label in ticklabels:
+            label.set_fontsize(10)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.streamplot(
+                x=np.arange(0, self.spacetime.shape[0]),
+                y=np.arange(0, self.spacetime.shape[1]),
+                u=self.vy,
+                v=self.vx,
+                linewidth=1,
+                cmap='jet',
+                density=3,
+                arrowstyle='->',
+                arrowsize=1.5
+            )
+        plt.show()
+
+
+    def animate(self, frames, fps=30, save=False):
+        frame = []
+        vector_field_X = []
+        vector_field_Y = []
+        frame.append(self.spacetime)
+        for i in tqdm(range(frames)):
+            self.next_time()
+            frame.append(self.spacetime)
+        fig = plt.figure()
+        ax = plt.axes()
+        im=plt.imshow(frame[0],interpolation='none', aspect='equal', cmap='inferno', vmin=np.min(frame[0]), vmax=np.max(frame[0]), origin='lower')
+        txt=plt.suptitle(f'{self.gini(frame[0])}')
+        
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+        # animation function.  This is called sequentially
+        def update(i):
+            im.set_array(frame[i])
+            txt.set_text(self.gini(frame[i]))
+            print(self.total_mass(frame[i]))
+            return [im, txt]
+
+        anim = FuncAnimation(
+                               fig, 
+                               update, 
+                               frames = frames,
+                               interval = 1000/fps, # in ms
+                               )
+        
+        print(f'Animating {int(frames)} frames - ({int(frames*self.tres)} seconds)')
+        plt.show()
+        if save:
+            anim.save('test_anim.mp4', fps=fps)
+    
+    def next_time(self):
+        next_spacetime = self.spacetime.copy()
+        next_vx = self.vx.copy()
+        next_vy = self.vy.copy()
+        for x1 in range(self.grid[0]):
+            for y1 in range(self.grid[1]):
+                for x2 in range(self.grid[0]):
+                    for y2 in range(self.grid[1]):
+                        #print(f'({x1},{y1}) : ({x2},{y2})')
+                        if x1 != x2 or y1 != y2:
+                            xx1, xx2, yy1, yy2 = x1*self.res, x2*self.res, y1*self.res, y2*self.res
+                            angle = np.arctan2((yy2-yy1),(xx2-xx1))
+                            acc = G*self.spacetime[x2,y2]/(distance(xx1,yy1,xx2,yy2)**2)
+                            self.vx[x1,y1] += np.cos(angle)*acc*self.tres
+                            self.vy[x1,y1] += np.sin(angle)*acc*self.tres
+                            #if (x1==0 and y1==0):
+                                #print(f'({x2},{y2}):', acc)
+                #VX
+                if self.vx[x1,y1] > 0:
+                    if x1 != self.grid[0]-1:
+                        dm = self.spacetime[x1,y1]*np.abs(self.vx[x1,y1])/self.res
+                        next_spacetime[x1+1,y1] += dm
+                        next_spacetime[x1,y1] -= dm
+                        next_vx[x1+1,y1] += (dm*self.vx[x1,y1]+self.spacetime[x1+1,y1]*self.vx[x1+1,y1])/(dm+self.spacetime[x1+1,y1])
+                else:
+                    if x1 != 0:
+                        dm = self.spacetime[x1,y1]*np.abs(self.vx[x1,y1])/self.res
+                        next_spacetime[x1-1,y1] += dm
+                        next_spacetime[x1,y1] -= dm
+                        next_vx[x1-1,y1] += (dm*self.vx[x1,y1]+self.spacetime[x1-1,y1]*self.vx[x1-1,y1])/(dm+self.spacetime[x1-1,y1])
+                #VY
+                if self.vy[x1,y1] > 0:
+                    if y1 != self.grid[1]-1:
+                        dm = self.spacetime[x1,y1]*np.abs(self.vy[x1,y1])/self.res
+                        next_spacetime[x1,y1+1] += dm
+                        next_spacetime[x1,y1] -= dm
+                        next_vy[x1,y1+1] += (dm*self.vy[x1,y1]+self.spacetime[x1,y1+1]*self.vy[x1,y1+1])/(dm+self.spacetime[x1,y1+1])
+                else:
+                    if y1 != 0:
+                        dm = self.spacetime[x1,y1]*np.abs(self.vy[x1,y1])/self.res
+                        next_spacetime[x1,y1-1] += dm
+                        next_spacetime[x1,y1] -= dm
+                        next_vx[x1,y1-1] += (dm*self.vy[x1,y1]+self.spacetime[x1,y1-1]*self.vy[x1,y1-1])/(dm+self.spacetime[x1,y1-1])
+
+                #Make sure the speed does not accumulate towards the outside of our universe
+                if x1 == 0 and self.vx[x1,y1] < 0:
+                    self.vx[x1,y1] = 0
+                if x1 == self.grid[0]-1 and self.vx[x1,y1] > 0:
+                    self.vx[x1,y1] = 0
+                if y1 == 0 and self.vy[x1,y1] < 0:
+                    self.vy[x1,y1] = 0
+                if y1 == self.grid[1] and self.vy[x1,y1] > 0:
+                    self.vy[x1,y1] = 0
+                    
+        self.spacetime = next_spacetime
+
+    def total_mass(self, state=None):
+        res = np.sum(state)
+        return res
+    
+    def gini(self, state=None):
+        if state is None:
+            state = self.spacetime
+        ordered_mass = np.sort(state.copy().flatten())
+        gini = 0
+        M = self.total_mass(state)
+        n = len(ordered_mass)
+        for i in range(1, n+1):
+            gini += (2*i-n-1)*np.abs(ordered_mass[i-1])
+        return gini/(np.abs(M/n)*n*(n-1))
+
+
+
+
+
+universe = Universe(20, 20, 1000, 1, sigma=500, tres=5000)
+#print(universe.gini())
+#universe.spacetime = np.zeros(universe.spacetime.shape)
+#universe.spacetime[4, 5] += 10000
+#universe.spacetime[8, 8] += 10000
+universe.next_time()
+universe.show()
+universe.animate(100, fps=10, save=True)
