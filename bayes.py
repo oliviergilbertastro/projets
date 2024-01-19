04db6ce7e6db452b64fcbb4bd8fe30ebcd10d9d9
OlivierGilbert <olgil12@ulaval.ca>
2024-01-19 10:19:47 -0500

    idk

--- bayes.py
+++ bayes.py
@@ -34,7 +34,7 @@ class Population():
 
 
 
-population = Population(1000000, 0.001, fiability=0.99)
+population = Population(10000000, 0.001, fiability=0.99)
 
 
 print('----------DOING FIRST TESTS----------')
