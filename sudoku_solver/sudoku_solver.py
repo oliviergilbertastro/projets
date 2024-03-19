bed3fd6241e7af20461d6fd2469368f6148635a4
Olivier Gilbert <olivier.gilbert.6@ulaval.ca>
2024-03-18 23:35:16 -0400

    solver finished

--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -12,6 +12,7 @@ def remove_blanks(a, blank='-'):
 class Sudoku():
     def __init__(self, grid, blank='-'):
         self.grid = np.array(grid)
+        self.startgrid = self.grid.copy()
         self.blank = blank
         if self.grid.shape != (9,9):
             raise ValueError("Grid is not 9x9")
@@ -31,7 +32,7 @@ class Sudoku():
         y,x = coord
         return remove_blanks(self.grid[((y)//3)*3:((y)//3+1)*3,((x)//3)*3:((x)//3+1)*3].flatten(), self.blank)
 
-    def solve_iteration(self):
+    def solve_iteration_basic(self):
         allpossibilities = set(['1', '2', '3', '4', '5', '6', '7', '8', '9'])
         for y in range(9):
             for x in range(9):
@@ -42,6 +43,19 @@ class Sudoku():
                     if len(possibilities) == 1:
                         self.grid[y,x] = possibilities[0]
 
+    def solve_iteration_guess(self, min=2):
+        allpossibilities = set(['1', '2', '3', '4', '5', '6', '7', '8', '9'])
+        for y in range(9):
+            for x in range(9):
+                if self.grid[y,x] == self.blank:
+                    interdictions = sorted(set(self.get_hline((y,x))+self.get_vline((y,x))+self.get_box((y,x))))
+                    possibilities = list(allpossibilities-set(interdictions))
+                    #print((x,y), possibilities)
+                    
+                    if len(possibilities) == min:
+                        self.grid[y,x] = possibilities[int(np.random.random()*min)]
+                        min = 1
+
     def solve_puzzle(self):
         unsolved = True
         iter = 0
@@ -49,15 +63,25 @@ class Sudoku():
         while unsolved:
             thisgrid = self.grid.copy()
             iter += 1
+            if int((1-list(thisgrid.flatten()).count(self.blank)/81)) == 1:
+                print('Puzzle already solved')
+                break
+            print(iter, f'{int((1-list(thisgrid.flatten()).count(self.blank)/81)*100)}% solved')
             if method == 0:
                 #Easiest method: check all possibilities
-                self.solve_iteration()
+                self.solve_iteration_basic()
                 if self.blank not in list(self.grid.flatten()):
                     unsolved = False
                 if np.array_equal(self.grid, thisgrid):
-                    print('This grid cannot be solved using this resolution method.')
-                    unsolved = False
-            print(iter)
+                    #print('This grid cannot be solved using this resolution method.')
+                    method += 1
+            elif method == 1:
+                #Dumbest method: guess when 2 possibilities
+                self.solve_iteration_guess(min=2)
+                if np.array_equal(self.grid, thisgrid):
+                    self.grid = self.startgrid.copy()
+                method = 0
+
         return iter
 
 
--- sudoku_solver/sudoku_test.py
+++ sudoku_solver/sudoku_test.py
@@ -5,7 +5,8 @@ from sudoku_solver import Sudoku
 from sudoky_converter import *
 
 
-grille = Sudoku(read_picture('sudoku_solver/pictures/sudoku7.png', show=True))
+grille = Sudoku(read_picture('sudoku_solver/pictures/sudoku8.png', show=True))
 
 grille.show()
+grille.solve_puzzle()
 grille.show()
