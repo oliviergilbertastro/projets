89e27815d666bfeaea78dc78a670beae51bbc767
Olivier Gilbert <olivier.gilbert.6@ulaval.ca>
2024-03-18 23:03:18 -0400

    sudoku solver easy

--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -2,9 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+def remove_blanks(a, blank='-'):
+    x = []
+    for i in a:
+        if i != blank:
+            x.append(i)
+    return (x)
+
 class Sudoku():
-    def __init__(self, grid):
+    def __init__(self, grid, blank='-'):
         self.grid = np.array(grid)
+        self.blank = blank
         if self.grid.shape != (9,9):
             raise ValueError("Grid is not 9x9")
 
@@ -13,17 +21,42 @@ class Sudoku():
 
     def get_hline(self, coord):
         y,x = coord
-        return self.grid[y,:]
+        return  remove_blanks(self.grid[y,:], self.blank)
 
     def get_vline(self, coord):
         y,x = coord
-        return self.grid[:,x]
+        return remove_blanks(self.grid[:,x], self.blank)
 
     def get_box(self, coord):
         y,x = coord
-        return self.grid[((y)//3)*3:((y)//3+1)*3,((x)//3)*3:((x)//3+1)*3].flatten()
+        return remove_blanks(self.grid[((y)//3)*3:((y)//3+1)*3,((x)//3)*3:((x)//3+1)*3].flatten(), self.blank)
 
     def solve_iteration(self):
-        pass
+        allpossibilities = set(['1', '2', '3', '4', '5', '6', '7', '8', '9'])
+        for y in range(9):
+            for x in range(9):
+                if self.grid[y,x] == self.blank:
+                    interdictions = sorted(set(self.get_hline((y,x))+self.get_vline((y,x))+self.get_box((y,x))))
+                    possibilities = list(allpossibilities-set(interdictions))
+                    #print((x,y), possibilities)
+                    if len(possibilities) == 1:
+                        self.grid[y,x] = possibilities[0]
+
+    def solve_puzzle(self):
+        unsolved = True
+        iter = 0
+        method = 0
+        while unsolved:
+            thisgrid = self.grid.copy()
+            iter += 1
+            if method == 0:
+                self.solve_iteration()
+                if self.blank not in list(self.grid.flatten()):
+                    unsolved = False
+                if np.array_equal(self.grid, thisgrid):
+                    print('This grid cannot be solved using this resolution method.')
+                    unsolved = False
+            print(iter)
+        return iter
 
 
--- sudoku_solver/sudoku_test.py
+++ sudoku_solver/sudoku_test.py
@@ -5,23 +5,7 @@ from sudoku_solver import Sudoku
 from sudoky_converter import *
 
 
-grille = Sudoku([
-    ['3','4','-','-','-','-','-','7','-'],
-    ['8','-','-','4','-','7','2','5','-'],
-    ['7','-','-','8','-','-','3','-','9'],
-    ['-','1','3','-','-','6','4','-','-'],
-    ['-','-','7','-','-','4','-','1','-'],
-    ['-','-','4','-','-','-','6','-','3'],
-    ['-','7','9','6','5','-','1','-','2'],
-    ['-','-','-','7','-','-','5','9','8'],
-    ['-','3','-','2','9','1','7','-','-'],
-    ]
-)
-
-grille = Sudoku(read_picture('sudoku_solver/pictures/sudoku4.png'))
+grille = Sudoku(read_picture('sudoku_solver/pictures/sudoku7.png', show=True))
 
 grille.show()
-#print(grille.get_hline((4,3)))
-#print(grille.get_vline((4,3)))
-print('box:')
-print(grille.get_box((6,6)))
+grille.show()
