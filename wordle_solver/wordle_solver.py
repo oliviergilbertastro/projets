7696e85122684042f79dda4d99461cf55853e2a2
OlivierGilbert <olgil12@ulaval.ca>
2025-01-08 14:05:19 -0500

    tests

--- wordle_solver/wordle_solver.py
+++ wordle_solver/wordle_solver.py
@@ -19,7 +19,7 @@ def placed_letter(letter, position, dictionnary):
             valid_words.append(dictionnary[i])
     return valid_words
 
-def unplaced_letter(letter, dictionnary):
+def unplaced_letter(letter, position, dictionnary):
     """
     letter: letter you have information on
     dictionnary: list of words currently possible
@@ -29,7 +29,7 @@ def unplaced_letter(letter, dictionnary):
     for i in range(len(dictionnary)):
         letter_in_word = False
         for k in range(len(dictionnary[i])):
-            if dictionnary[i][k] == letter:
+            if dictionnary[i][k] == letter and k != position:
                 letter_in_word = True
         if letter_in_word:
             valid_words.append(dictionnary[i])
@@ -60,12 +60,22 @@ def bad_letter(letter, dictionnary):
 
 
 if __name__ == "__main__":
-    words = get_words()
+    words, probs = get_words()
 
     # Start reducing sample for example word "DRAFT"
     # Let's say we try STAGE
+    print(len(words))
     words = placed_letter("A", 2, words)
-    words = unplaced_letter("T", words)
-    words = bad_letter("A", words)
-    words = bad_letter("H", words)
+    print(len(words))
+    words = unplaced_letter("T", 1, words)
+    print(len(words))
+    words = bad_letter("SGE", words)
+    print(len(words))
+    # Let's try TRACK
+    words = unplaced_letter("T", 0, words)
+    print(len(words))
+    words = placed_letter("R", 1, words)
+    print(len(words))
+    words = bad_letter("CK", words)
+    print(len(words))
     print(words)
