9f06e0411d90f99859aa6eb4dcac7f8a221fefec
OlivierGilbert <olgil12@ulaval.ca>
2025-01-29 16:22:19 -0500

    show

--- queens_solver/queens_solver.py
+++ queens_solver/queens_solver.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
+from matplotlib.patches import Rectangle
 
 class Solver:
     def __init__(self, grid, colors):
@@ -26,27 +27,29 @@ class Solver:
     
     def show(self):
         fig = plt.gcf()
-        fig.set_size_inches(6, 6)
+        fig.set_size_inches(6/9*self.n, 6/9*self.n)
         ax1 = plt.subplot(111)
         ticklabels = ax1.get_xticklabels()
         ticklabels.extend( ax1.get_yticklabels() )
         for label in ticklabels:
             label.set_fontsize(0)
-        for i in range(10):
-            lw = 1
-            if i%3 == 0:
-                lw = 5
+        for i in range(self.n+1):
+            lw = 2
             plt.axvline(i*50, ymin=0, ymax=1, color='black', linewidth=lw)
             plt.axhline(i*50, xmin=0, xmax=1, color='black', linewidth=lw)
-        for y in range(9):
-            for x in range(9):
-                if self.startgrid[y,x] != self.blank:
-                    rect = Rectangle((x*50, y*50), 50, 50, color='black', alpha=0.25, fill=True)
-                    ax1.add_patch(rect)
-                if self.grid[y,x] != self.blank:
-                    plt.text((x)*50+12.5, (y)*50+39, self.grid[y,x], fontsize=30)
-        plt.xlim(0, 450)
-        plt.ylim(450, 0)
+        for y in range(self.n):
+            for x in range(self.n):
+                rect = Rectangle((x*50, y*50), 50, 50, color=self.colors[self.grid[y,x]], alpha=0.8, fill=True)
+                ax1.add_patch(rect)
+                
+                #if self.startgrid[y,x] != self.blank:
+                #    rect = Rectangle((x*50, y*50), 50, 50, color='black', alpha=0.25, fill=True)
+                #    ax1.add_patch(rect)
+                #if self.grid[y,x] != self.blank:
+                #    plt.text((x)*50+12.5, (y)*50+39, self.grid[y,x], fontsize=30)
+                pass
+        plt.xlim(0, 50*self.n)
+        plt.ylim(50*self.n, 0)
         plt.show()
 
     def solve_iteration(self):
@@ -65,3 +68,4 @@ if __name__ == "__main__":
                      ])
     colors = ["red","blue","green","orange","purple"]
     Queens = Solver(grid, colors)
+    Queens.show()
