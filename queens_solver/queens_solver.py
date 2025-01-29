3d61498864454c207fe1f95567ee60845fb728e8
OlivierGilbert <olgil12@ulaval.ca>
2025-01-29 16:14:39 -0500

    start of queens solver

---
+++ queens_solver/queens_solver.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+
+class Solver:
+    def __init__(self, grid, colors):
+        assert grid.shape[0] == grid.shape[1] == len(colors)
+        self.grid = grid # np.array of numbers representing colors
+        self.colors = colors
+        self.initial_grid = copy.deepcopy(grid)
+        self.n = grid.shape[0]
+        self.crowns = []
+        self.color_indices = self.get_color_indices()
+        assert len(self.color_indices) == self.n
+
+    def get_color_indices(self):
+        colors = {}
+        for y in range(self.n):
+            for x in range(self.n):
+                if self.grid[y,x] in colors:
+                    colors[self.grid[y,x]].append((y,x))
+                else:
+                    colors[self.grid[y,x]] = [(y,x)]
+        return colors
+            
+    
+    def show(self):
+        fig = plt.gcf()
+        fig.set_size_inches(6, 6)
+        ax1 = plt.subplot(111)
+        ticklabels = ax1.get_xticklabels()
+        ticklabels.extend( ax1.get_yticklabels() )
+        for label in ticklabels:
+            label.set_fontsize(0)
+        for i in range(10):
+            lw = 1
+            if i%3 == 0:
+                lw = 5
+            plt.axvline(i*50, ymin=0, ymax=1, color='black', linewidth=lw)
+            plt.axhline(i*50, xmin=0, xmax=1, color='black', linewidth=lw)
+        for y in range(9):
+            for x in range(9):
+                if self.startgrid[y,x] != self.blank:
+                    rect = Rectangle((x*50, y*50), 50, 50, color='black', alpha=0.25, fill=True)
+                    ax1.add_patch(rect)
+                if self.grid[y,x] != self.blank:
+                    plt.text((x)*50+12.5, (y)*50+39, self.grid[y,x], fontsize=30)
+        plt.xlim(0, 450)
+        plt.ylim(450, 0)
+        plt.show()
+
+    def solve_iteration(self):
+        # Check all the grid
+        for y in range(self.n):
+            for x in range(self.n):
+                pass
+
+
+if __name__ == "__main__":
+    grid = np.array([[0,0,1,1,1],
+                     [0,1,1,1,1],
+                     [0,2,2,1,1],
+                     [0,3,3,3,3],
+                     [0,3,4,4,3],
+                     ])
+    colors = ["red","blue","green","orange","purple"]
+    Queens = Solver(grid, colors)
