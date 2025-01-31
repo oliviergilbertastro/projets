4703022020f7497aacd108bbe18ffcae3d7231f2
Olivier Gilbert <olivier.gilbert.6@ulaval.ca>
2025-01-31 00:38:32 -0500

    wordle!

--- wordle_helper/play.py
+++ wordle_helper/play.py
@@ -3,9 +3,10 @@ from wordle_solver_v2 import WordleSolver
 
 N_LETTERS = 5
 LANGUAGE = "english"
-#LANGUAGE = "french"
-#LANGUAGE = "german"
+LANGUAGE = "french"
+LANGUAGE = "german"
 
-
-Wordle = WordleSolver(get_data(n_letters=N_LETTERS, language=LANGUAGE))
+dic = get_data(n_letters=N_LETTERS, language=LANGUAGE)
+print(len(dic[0]))
+Wordle = WordleSolver(dic)
 Wordle.play_console(if_plot=True, inputs="console")
